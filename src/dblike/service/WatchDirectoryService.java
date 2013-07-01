ae797d0c2010ff555db6e248a324a4afa0584514
nicole <nicoleyu2003@gmail.com>
2013-06-30 23:06:23 -0700

    add watch folder changes service

---
+++ src/dblike/service/WatchDirectoryService.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dblike.service;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+/**
+ *
+ * @author JingboYu
+ */
+public class WatchDirectoryService {
+    
+    private final WatchService watchService;
+    private final Path directory;
+    
+    public WatchDirectoryService(String searchDir) throws IOException {
+        
+        FileSystem fs = FileSystems.getDefault();
+        watchService = fs.newWatchService();
+        directory = fs.getPath(searchDir);
+        directory.register(watchService, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE, OVERFLOW);
+        System.out.println("Registered watchService on " + directory);
+        
+    }
+    
+    public void watchFile() throws IOException {
+        
+        for (;;) {
+            
+            WatchKey key;
+            try {
+                key = watchService.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+            
+            for (WatchEvent<?> e : key.pollEvents()) {
+                
+                @SuppressWarnings("unchecked")
+                        WatchEvent<Path> event = (WatchEvent<Path>) e;
+                
+                Path fileName = event.context();
+                Path child = directory.resolve(fileName);
+                String contentType = Files.probeContentType(child);
+                
+                if (e.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+                    System.out.println("file was created: " + fileName + " contentType: " + contentType);
+                } else if (e.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+                    System.out.println("file was modified: " + fileName + " contentType: " + contentType);
+                } else if (e.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+                    System.out.println("file was deleted: " + fileName);
+                } else if (e.kind() == StandardWatchEventKinds.OVERFLOW) {
+                    System.out.println("overflow occurred");
+                    continue;
+                }
+                
+                boolean valid = key.reset();
+                if (!valid) {
+                    System.out.println("object no longer registered");
+                    break;
+                }
+                
+            }
+        }
+        
+    }
+    
+}
---
+++ src/dblike/service/test/WatchDirectoryServiceTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dblike.service.test;
+
+import dblike.service.WatchDirectoryService;
+import java.io.IOException;
+
+/**
+ *
+ * @author JingboYu
+ */
+public class WatchDirectoryServiceTest {
+    
+    public static void main(String[] args) throws IOException {
+        new WatchDirectoryService("E:\\Dropbox\\Course\\CICS525\\dblike\\test\\").watchFile();
+    }
+}
