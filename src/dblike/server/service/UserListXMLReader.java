8fef0963da9863e6ae4ddd653b49d83d8302803b
nicole <nicoleyu2003@gmail.com>
2013-07-08 16:50:45 -0700

    move UserListXMLReader class to service package

--- src/dblike/server/service/UserListXMLReader.java
+++ src/dblike/server/service/UserListXMLReader.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package dblike.server;
+package dblike.server.service;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
