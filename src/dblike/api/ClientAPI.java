485af86baa8a497ae71fc653cb4cde6cf4a2cba5
Laan <ericbhu@gmail.com>
2013-07-15 16:11:45 -0700

    add interface functions of ServerAPI and ClientAPI

--- src/dblike/api/ClientAPI.java
+++ src/dblike/api/ClientAPI.java
@@ -6,6 +6,7 @@ package dblike.api;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */ 
+import dblike.service.FileInfo;
 import java.rmi.*;
 
 /**
@@ -16,4 +17,8 @@ public interface ClientAPI extends Remote {
  
     public void showMessage(String mmm) throws RemoteException;
     public boolean beatFromServer(String serverIP, int port) throws RemoteException;
+    
+    public FileInfo getFileInfoFromServer(String serverIP, int port, String userName, String directory, String fileName) throws RemoteException;
+    
+    public void setFileInfoToServer(String serverIP, int port, String userName, String directory, String fileName, FileInfo fileInfo) throws RemoteException;
 }
