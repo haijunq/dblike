1c2dad551c303ad00d9fe9bdc1f35f2a07b85e40
nicoleyu2003 <nicoleyu2003@gmail.com>
2013-07-17 13:32:21 -0700

    add two methods to avoid null pointer exception in ServerAPI

--- src/dblike/server/service/FileListService.java
+++ src/dblike/server/service/FileListService.java
@@ -13,55 +13,65 @@ import java.util.Hashtable;
  * @author haijun
  */
 public final class FileListService {
-
-    private String pathname; //username
-    private Hashtable<String, FileInfo> fileHashTable ; 
     
-   
-//    public FileListService getInstance() {
-//        if (fileHashTable == null) {
-//            fileHashTable = new Hashtable<String, byte []>();
-//        }
-//        return fileHashTable;
-//    }
-
+    private String pathname; //username
+    private Hashtable<String, FileInfo> fileHashTable ;
+    
+    
+    //    public FileListService getInstance() {
+    //        if (fileHashTable == null) {
+    //            fileHashTable = new Hashtable<String, byte []>();
+    //        }
+    //        return fileHashTable;
+    //    }
+    
     
     //every user needs a FileListService object, so I removed "static"
-//        if (fileHashTable == null) {
-//            fileHashTable = new Hashtable<String, byte []>();
-//        }
-//        return fileHashTable;
-//    }
-
+    //        if (fileHashTable == null) {
+    //            fileHashTable = new Hashtable<String, byte []>();
+    //        }
+    //        return fileHashTable;
+    //    }
+    
     
     public String getPathname() {
         return pathname;
     }
-
+    
     public void setPathname(String pathname) {
         this.pathname = pathname;
     }
-
+    
     public FileListService() {
         this.pathname = "";
         this.fileHashTable = new Hashtable<>();
     }
-
+    
     
     
     public FileListService(String username) {
         this.pathname = "./users/" + username + "/";
         fileHashTable = new Hashtable<>();
     }
-
+    
     public Hashtable<String, FileInfo> getFileHashTable() {
         return this.fileHashTable;
     }
-
+    
     public void setFileHashTable(Hashtable<String, FileInfo> fileHashTable) {
         this.fileHashTable = fileHashTable;
     }
-
+    
+    public boolean containFileInfo(String fileName) {
+        
+        return fileHashTable.containsKey(fileName);
+    }
+    
+    public FileInfo getFileInfo(String fileName) {
+        
+        return fileHashTable.get(fileName);
+    }
+    
     public FileInfo getFileInfoByFileName(String fileName) {
         
         if (fileHashTable.containsKey(fileName))
@@ -77,10 +87,10 @@ public final class FileListService {
     
     public void addNewFileInfo(FileInfo newFileInfo) {
         this.fileHashTable.put(newFileInfo.getFileName(), newFileInfo);
-    } 
+    }
     
     public void updateFileInfo(FileInfo newFileInfo) {
-        this.fileHashTable.put(newFileInfo.getFileName(), newFileInfo);        
+        this.fileHashTable.put(newFileInfo.getFileName(), newFileInfo);
     }
     
     public void removeFileInfo(String fileName) {
